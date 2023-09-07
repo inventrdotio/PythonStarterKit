7bde7a107858ff86c7b2cfa1f592395a24df0236
InventrKits <120519839+inventrdotio@users.noreply.github.com>
2023-09-07 10:04:20 -0400

    Update 02-morse_code.py

--- 2-Morse_Code/02-morse_code.py
+++ 2-Morse_Code/02-morse_code.py
@@ -1,40 +1,63 @@
 import machine
-import utime
+import time
 
-# Initialize GPIO pins for LED and Button
-led = machine.Pin(14, machine.Pin.OUT)
-button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+# Define the GPIO pin for the button
+button_pin = 15
+button = machine.Pin(button_pin, machine.Pin.IN, machine.Pin.PULL_UP)
 
-# Constants for Morse Code
-DOT_LENGTH = 200  # 200ms for dot
-DASH_LENGTH = 600  # 600ms for dash
-PAUSE_LENGTH = 200  # 200ms pause between dots and dashes
-LONG_PAUSE_LENGTH = 600  # 600ms pause between letters
+# Morse code mapping
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--',
+    '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
+    '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
 
-# Function to blink the LED
-def blink(duration):
-    led.value(1)
-    utime.sleep_ms(duration)
-    led.value(0)
-    utime.sleep_ms(PAUSE_LENGTH)
+dot_duration = 0.2
+dash_duration = 0.6
+end_of_letter_duration = 1.5
 
-# Main Loop
-while True:
-    if button.value() == 0:  # Button pressed (pull-down)
-        press_time = utime.ticks_ms()  # Record the time when button is pressed
-        
-        # Wait for button to be released
+def interpret_button_press():
+    morse_string = ''
+    while True:
+        while button.value() == 1:
+            time.sleep(0.05)
+
+        # Measure the duration of the button press
+        start_time = time.time()
         while button.value() == 0:
             pass
-        
-        # Calculate how long the button was held down
-        hold_time = utime.ticks_diff(utime.ticks_ms(), press_time)
-        
-        # Short press for dot
-        if hold_time < 400:
-            blink(DOT_LENGTH)
-        # Long press for dash
+        end_time = time.time()
+        duration = end_time - start_time
+
+        if duration <= dot_duration:
+            morse_string += '.'
         else:
-            blink(DASH_LENGTH)
+            morse_string += '-'
         
-        utime.sleep_ms(LONG_PAUSE_LENGTH)
+        # Wait to see if the user finishes inputting the letter
+        start_time = time.time()
+        while button.value() == 1:
+            if (time.time() - start_time) > end_of_letter_duration:
+                return morse_string
+
+def main():
+    print("Input Morse code using the button. A short press is '.', a long press is '-'.")
+    print("Wait for more than 1.5 seconds to finish inputting a letter.")
+    
+    while True:
+        morse_string = interpret_button_press()
+        for key, value in morse_code.items():
+            if value == morse_string:
+                print(key)
+                break
+        else:
+            print(f"Unrecognized Morse code: {morse_string}")
+
+if __name__ == "__main__":
+    main()
+
