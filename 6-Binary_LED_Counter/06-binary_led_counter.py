0d5a782c120e76ab6bafeb0ab14aa3a5f43d2c25
InventrKits <120519839+inventrdotio@users.noreply.github.com>
2023-09-11 07:53:15 -0400

    Update 06-binary_led_counter.py

--- 6-Binary_LED_Counter/06-binary_led_counter.py
+++ 6-Binary_LED_Counter/06-binary_led_counter.py
@@ -1,26 +1,44 @@
 import machine
-import utime
+import time
 
-# Initialize button
-button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+# Define GPIO pins for the 4 LEDs
+led_pins = [
+    machine.Pin(0, machine.Pin.OUT),
+    machine.Pin(1, machine.Pin.OUT),
+    machine.Pin(2, machine.Pin.OUT),
+    machine.Pin(3, machine.Pin.OUT)
+]
+button_pin = machine.Pin(8, machine.Pin.IN, machine.Pin.PULL_UP)
 
-# Initialize LEDs
-led_pins = [14, 16, 17, 18]
-leds = [machine.Pin(pin, machine.Pin.OUT) for pin in led_pins]
+# Initialize LED states for 4 LEDs
+led_states = [0] * len(led_pins)
 
-# Initialize counter
-counter = 0
+# Function to update LED states
+def update_led_states():
+    for i, led in enumerate(led_pins):
+        if led_states[i] == 1:
+            led.on()
+        else:
+            led.off()
 
-# Function to display counter in binary on LEDs
-def display_counter(counter):
-    for i in range(4):
-        leds[i].value((counter >> i) & 1)
+# Function to increment binary counter
+def increment_counter():
+    # Convert LED states to integer
+    binary_value = int(''.join(map(str, led_states)), 2)
+    binary_value += 1
+    # If value is 16 (10000 in binary), reset to 0
+    if binary_value >= 16:
+        binary_value = 0
+    binary_string = bin(binary_value)[2:]  # Convert decimal to binary string
+    binary_string = binary_string.zfill(len(led_pins))  # Pad with zeros if necessary
 
-# Main Loop
+    # Update LED states
+    for i, bit in enumerate(binary_string):
+        led_states[i] = int(bit)
+
+# Main loop
 while True:
-    if button.value() == 0:  # Button pressed
-        counter = (counter + 1) % 16  # Increment counter, reset to 0 if it reaches 16
-        display_counter(counter)  # Update LED display
-        utime.sleep_ms(200)  # Debounce button
-        
-    utime.sleep_ms(10)
+    if button_pin.value() == 0:
+        increment_counter()
+        update_led_states()
+        time.sleep(0.2)  # Button debounce delay
