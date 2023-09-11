7529381b0ce6c43f234127c5cdce8f0b04de85dd
InventrKits <120519839+inventrdotio@users.noreply.github.com>
2023-09-11 08:50:38 -0400

    Update 07-analog-light-pwm.py
    pin changes

--- 7-Analog_Light_PWM/07-analog-light-pwm.py
+++ 7-Analog_Light_PWM/07-analog-light-pwm.py
@@ -2,10 +2,10 @@ import machine
 import utime
 
 # Initialize button on GPIO 15
-button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+button = machine.Pin(1, machine.Pin.IN, machine.Pin.PULL_UP)
 
 # Initialize LED on GPIO 14 as a PWM pin
-led = machine.PWM(machine.Pin(14))
+led = machine.PWM(machine.Pin(0))
 
 # Predefined brightness levels (0-65535)
 brightness_levels = [0, 8192, 16384, 32768, 65535]
